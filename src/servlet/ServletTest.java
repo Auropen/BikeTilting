5b5a879a7a2760a0e31ec343801e00f663d1cc7a
Auropen <sonixbock@hotmail.com>
2015-12-01 11:46:00 +0100

    Setted the project structure up :squirrel:

--- src/servlet/ServletTest.java
+++ src/servlet/ServletTest.java
@@ -1,3 +1,4 @@
+package servlet;
 
 
 import java.io.IOException;
@@ -19,14 +20,12 @@ public class ServletTest extends HttpServlet {
      */
     public ServletTest() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
@@ -34,8 +33,6 @@ public class ServletTest extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-
 }
