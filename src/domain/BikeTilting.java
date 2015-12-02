2102af59c8e212281c6ee542397037930601dd8b
kornel1984 <kornelus84@gmail.com>
2015-12-02 14:56:57 +0100

    :fu: you

--- src/domain/BikeTilting.java
+++ src/domain/BikeTilting.java
@@ -14,6 +14,13 @@ import java.util.List;
  */
 public class BikeTilting {
 
+    /**
+     * @param aInstance the instance to set
+     */
+    public static void setInstance(BikeTilting aInstance) {
+        instance = aInstance;
+    }
+
     private List<User> users;
     private List<Participant> participants;
     private List<Lane> lanes;
@@ -33,25 +40,67 @@ public class BikeTilting {
     }
 
     public boolean createUser(String cpr, String fName, String lName, String email, String password, String phoneNumber, int accessLevel) {
-        users.add(new User(cpr, fName, lName, email, password, phoneNumber, accessLevel));
+        getUsers().add(new User(cpr, fName, lName, email, password, phoneNumber, accessLevel));
         return true;
     }
 
     public boolean createParticipant(String fName, String lName, String ageRange, String email, String scoreID, String colour, int laneNr) {
-        participants.add(new Participant(fName, lName, ageRange, email, scoreID, colour, laneNr));
+        getParticipants().add(new Participant(fName, lName, ageRange, email, scoreID, colour, laneNr));
         return true;
     }
 
     public boolean createLane(int laneNr) {
-        lanes.add(new Lane(laneNr));
+        getLanes().add(new Lane(laneNr));
         return true;
     }
     
-    public boolean addScoreHit(Participant p){
+    public void addScoreHit(Participant p){
         p.getScore().addHit();
     }
     
-    public boolean addScoreMiss(Participant p){
+    public void addScoreMiss(Participant p){
         p.getScore().addMiss();
+    }
+
+    /**
+     * @return the users
+     */
+    public List<User> getUsers() {
+        return users;
+    }
+
+    /**
+     * @param users the users to set
+     */
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    /**
+     * @return the participants
+     */
+    public List<Participant> getParticipants() {
+        return participants;
+    }
+
+    /**
+     * @param participants the participants to set
+     */
+    public void setParticipants(List<Participant> participants) {
+        this.participants = participants;
+    }
+
+    /**
+     * @return the lanes
+     */
+    public List<Lane> getLanes() {
+        return lanes;
+    }
+
+    /**
+     * @param lanes the lanes to set
+     */
+    public void setLanes(List<Lane> lanes) {
+        this.lanes = lanes;
     }
 }
