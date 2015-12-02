2d5b1b8a21ee77d74d6076e2445ff00040cca3b1
Auropen <sonixbock@hotmail.com>
2015-12-02 13:28:06 +0100

    More shit to the Score :squirrel:

--- src/domain/Score.java
+++ src/domain/Score.java
@@ -2,9 +2,13 @@ package domain;
 
 public class Score {
 	private int scoreID;
+	private int score;
+	private String hitScore;
 
-	public Score(int scoreID) {
+	public Score(int scoreID, int score, String hitScore) {
 		this.setScoreID(scoreID);
+		this.setScore(score);
+		this.setHitScore(hitScore);
 	}
 
 	public int getScoreID() {
@@ -13,5 +17,21 @@ public class Score {
 
 	public void setScoreID(int scoreID) {
 		this.scoreID = scoreID;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
+	public String getHitScore() {
+		return hitScore;
+	}
+
+	public void setHitScore(String hitScore) {
+		this.hitScore = hitScore;
 	}
 }
