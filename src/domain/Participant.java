ae1a67a69c560bc5eae76ca04f830155e227fd41
Donpeal666 <Donpeal666@gmail.com>
2015-12-02 10:27:34 +0100

    filled add user and participants methods

--- src/domain/Participant.java
+++ src/domain/Participant.java
@@ -1,40 +1,83 @@
 package domain;
 
 public class Participant {
+	
+	private String colour;
+	private String fName;
+	private String lName;
+	private String email;
+	private String ageRange;
+	private String scoreID;
+	private int laneNr;
 
-	public String getFName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public String getLName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public String getAgeRange() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public String getEmail() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public String getScoreID() {
-		// TODO Auto-generated method stub
-		return null;
+	public Participant(String fName, String lName, String ageRange, String email, String scoreID, String colour, int laneNr) {
+		
+		this.setFName(fName);
+		this.setLName(lName);
+		this.setEmail(email);
+		this.setAgeRange(ageRange);
+		this.setScoreID(scoreID);
+		this.setColour(colour);
+		this.setLaneNr(laneNr);
+		
 	}
 
 	public String getColour() {
-		// TODO Auto-generated method stub
-		return null;
+		return colour;
 	}
 
-	public String getLaneNr() {
-		// TODO Auto-generated method stub
-		return null;
+	public void setColour(String colour) {
+		this.colour = colour;
 	}
 
+	public String getFName() {
+		return fName;
+	}
+
+	public void setFName(String fName) {
+		this.fName = fName;
+	}
+
+	public String getLName() {
+		return lName;
+	}
+
+	public void setLName(String lName) {
+		this.lName = lName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAgeRange() {
+		return ageRange;
+	}
+
+	public void setAgeRange(String ageRange) {
+		this.ageRange = ageRange;
+	}
+
+	public String getScoreID() {
+		return scoreID;
+	}
+
+	public void setScoreID(String scoreID) {
+		this.scoreID = scoreID;
+	}
+
+	public int getLaneNr() {
+		return laneNr;
+	}
+
+	public void setLaneNr(int laneNr) {
+		this.laneNr = laneNr;
+	}
+	
+	
+	
 }
--- src/domain/User.java
+++ src/domain/User.java
@@ -2,44 +2,82 @@ package domain;
 
 public class User {
 	
-	public User() {
-		// TODO Auto-generated constructor stub
-	}
+	private String cpr;
+	private String fName;
+	private String lName;
+	private String email;
+	private String password;
+	private String phoneNumber;
+	private int accessLevel;
 
-	public String getAccessLevel() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public String getPhoneNumber() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public String getEmail() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public String getLName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public String getFName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public String getPassword() {
-		// TODO Auto-generated method stub
-		return null;
+	public User(String cpr, String fName, String lName, String email, String password, String phoneNumber, int accessLevel) {
+		this.setCPR(cpr);
+		this.setFName(fName);
+		this.setLName(lName);
+		this.setEmail(email);
+		this.setPassword(password);
+		this.setPhoneNumber(phoneNumber);
+		this.setAccessLevel(accessLevel);
+		
 	}
 
 	public String getCPR() {
-		// TODO Auto-generated method stub
-		return null;
+		return cpr;
 	}
+
+	public void setCPR(String cpr) {
+		this.cpr = cpr;
+	}
+
+	public String getFName() {
+		return fName;
+	}
+
+	public void setFName(String fName) {
+		this.fName = fName;
+	}
+
+	public String getLName() {
+		return lName;
+	}
+
+	public void setLName(String lName) {
+		this.lName = lName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public int getAccessLevel() {
+		return accessLevel;
+	}
+
+	public void setAccessLevel(int accessLevel) {
+		this.accessLevel = accessLevel;
+	}
+	
+	
 	
 		
 }
