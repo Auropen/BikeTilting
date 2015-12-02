e05c437adc17d3e8de878dfaf24c6aaeeb826674
Auropen <sonixbock@hotmail.com>
2015-12-02 13:25:51 +0100

    Score ID added to the Score :squirrel:

--- src/domain/Score.java
+++ src/domain/Score.java
@@ -1,5 +1,17 @@
 package domain;
 
 public class Score {
+	private int scoreID;
 
+	public Score(int scoreID) {
+		this.setScoreID(scoreID);
+	}
+
+	public int getScoreID() {
+		return scoreID;
+	}
+
+	public void setScoreID(int scoreID) {
+		this.scoreID = scoreID;
+	}
 }
