d03b631e2ecee7c0e281ba978ac98a16c9fac70c
kornel1984 <kornelus84@gmail.com>
2015-12-02 13:32:39 +0100

    BikeTilting stuff :squirrel:

--- src/domain/BikeTilting.java
+++ src/domain/BikeTilting.java
@@ -16,19 +16,42 @@ public class BikeTilting {
 
     private List<User> users;
     private List<Participant> participants;
+    private List<Lane> lanes;
+    private static BikeTilting instance;
 
-    public BikeTilting() {
+    private BikeTilting() {
         this.users = new ArrayList<User>();
         this.participants = new ArrayList<Participant>();
+        this.lanes = new ArrayList<Lane>();
     }
 
-    private boolean createUser(String cpr, String fName, String lName, String email, String password, String phoneNumber, int accessLevel) {
+    public static BikeTilting getInstance() {
+        if (instance == null) {
+            instance = new BikeTilting();
+        }
+        return instance;
+    }
+
+    public boolean createUser(String cpr, String fName, String lName, String email, String password, String phoneNumber, int accessLevel) {
         users.add(new User(cpr, fName, lName, email, password, phoneNumber, accessLevel));
         return true;
     }
 
-    private boolean createParticipant(String fName, String lName, String ageRange, String email, String scoreID, String colour, int laneNr) {
+    public boolean createParticipant(String fName, String lName, String ageRange, String email, String scoreID, String colour, int laneNr) {
         participants.add(new Participant(fName, lName, ageRange, email, scoreID, colour, laneNr));
         return true;
+    }
+
+    public boolean createLane(int laneNr) {
+        lanes.add(new Lane(laneNr));
+        return true;
+    }
+    
+    public boolean addScoreHit(Participant p){
+        p.getScore().addHit();
+    }
+    
+    public boolean addScoreMiss(Participant p){
+        p.getScore().addMiss();
     }
 }
--- src/domain/Lane.java
+++ src/domain/Lane.java
@@ -1,12 +1,16 @@
-
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lane {
     private int laneNr;
     private List<Participant> participants;
-            
+
+    public Lane(int laneNr) {
+        this.laneNr = laneNr;
+        this.participants = new ArrayList<Participant>();
+    }
     
     private void addParticipant(Participant p) {
         participants.add(p);
