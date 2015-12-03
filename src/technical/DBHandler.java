2f19228a2903079ce5376a5db61c16ce7a2bcdb2
Donpeal666 <Donpeal666@gmail.com>
2015-12-03 12:39:08 +0100

    DBHandler With storedProcedures FIX!!!!!

--- src/technical/DBHandler.java
+++ src/technical/DBHandler.java
@@ -3,6 +3,7 @@ package technical;
 import java.awt.Color;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -68,6 +69,7 @@ public class DBHandler {
 		}
 	}
 
+
 	public static DBHandler getInstance (){
 		if (instance == null) instance = new DBHandler();
 		return instance;
@@ -89,6 +91,249 @@ public class DBHandler {
 
 	public Connection getConnection() {
 		return connection;
+	}
+
+	
+	
+	
+
+	public User addUser(String cpr, String fName, String lName, String email, String password, String phoneNumber, int accessLevel) {
+
+		try {		
+			CallableStatement cs = connection.prepareCall("{call addParticipant(?,?,?,?,?,?,?)}");
+			ResultSet rs = cs.executeQuery();
+		
+			cs.setString(1,cpr);
+			cs.setString(2,fName);
+			cs.setString(3,lName);
+			cs.setString(4,email);
+			cs.setString(5,password);
+			cs.setString(6,phoneNumber);
+			cs.setInt(7,accessLevel);
+			
+			cs.execute();
+
+			return new User(cpr, fName, lName, email, password, phoneNumber, accessLevel);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+
+	public Participant addParticipant(String fName, String lName, String ageRange, String email, Score score, Color shirtColor, Integer shirtNumber) {
+
+		try {	
+			
+			CallableStatement cs = connection.prepareCall("{call addParticipant(?,?,?,?,?,?,?,?,?)}");
+			ResultSet rs = cs.executeQuery();
+			
+			cs.setString(1,fName);
+			cs.setString(2,lName);
+			cs.setString(3,ageRange);
+			cs.setString(4,email);
+			cs.setInt(5,score.getScoreID());
+			cs.setString(6,getColorString(shirtColor));
+			cs.setInt(7,shirtNumber);
+			
+			cs.registerOutParameter(9, java.sql.Types.INTEGER);
+			int participantID = cs.getInt(9);
+			
+			
+			
+			return new Participant(participantID, fName, lName, ageRange, email, score, shirtColor, shirtNumber);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public String createSQLQuery(String table, Object... data) {
+		String sql = "INSERT INTO " + table + "(";
+
+		for (int i = 0; i < data.length; i += 2) 
+			sql += data[i] + ",";
+
+		sql = sql.substring(0, sql.length()-1) + ") VALUES (";
+
+		for (int i = 1; i < data.length; i += 2) 
+			sql += ((isNumber(data[i])) ? data[i] : "'"+data[i]+"'") + ",";
+
+		return sql.substring(0, sql.length()-1) + ")";
+	}
+
+	private boolean isNumber(Object o) {
+		return o instanceof Byte || o instanceof Short || o instanceof Integer || o instanceof Long || o instanceof Double || o instanceof Float;
+	}
+
+	
+	
+	
+	
+	public List<User> getAllUsers() {
+
+		List<User> users = new ArrayList<>();
+
+		try {
+				
+			CallableStatement csUser = connection.prepareCall("{call getUser}");
+			ResultSet rsUser = csUser.executeQuery();
+			
+			while(rsUser.next()){
+				
+				User u = new User(rsUser.getString("fldCPR"), 
+								  rsUser.getString("fldPassword"), 
+								  rsUser.getString("fldFName"), 
+								  rsUser.getString("fldLName"), 
+								  rsUser.getString("fldEmail"), 
+								  rsUser.getString("fldPhoneNumber"), 
+								  rsUser.getInt("fldAccessLevel"));	
+			   users.add(u);
+				
+			}
+		
+			return users;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+	}
+	
+	public List<Participant> getAllParticipants() {
+
+		List<Participant> participants = new ArrayList<>();
+				
+		try {
+
+			CallableStatement cs = connection.prepareCall("{call getParticipant}");
+			
+			ResultSet rs = cs.executeQuery();
+			
+			while(rs.next()){
+				
+				int scoreID = rs.getInt("fldScoreID");
+				CallableStatement csScore = connection.prepareCall("{call getScoreByID(?)");
+				cs.setInt(1, scoreID);
+				ResultSet rsScore = csScore.executeQuery();
+				Score score = new Score(scoreID, rsScore.getInt("fldScore"), rsScore.getString("fldHitScore"));
+				
+				Participant p = new Participant(rs.getInt("fldParticipantID"),
+												rs.getString("fldFName"), 
+									            rs.getString("fldLName"),
+									            rs.getString("fldAgeRange"),
+									            rs.getString("fldEmail"), 
+									            score,
+									            getColor(rs.getString("fldShirtColour")),
+									            rs.getInt("fldShirtNumber"));	
+				
+				participants.add(p);
+				
+			}
+			
+			return participants;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+	}
+	
+	
+	public Participant getParticipants(int id) {
+
+		try {
+
+			CallableStatement cs = connection.prepareCall("{call getParticipant(?)}");
+			cs.setInt(1, id);
+			ResultSet rs = cs.executeQuery();
+			
+			while(rs.next()){
+				
+				int scoreID = rs.getInt("fldScoreID");
+				CallableStatement csScore = connection.prepareCall("{call getParticipant(?)}");
+				cs.setInt(1, scoreID);
+				
+				ResultSet rsScore = csScore.executeQuery();
+				rsScore.next();
+				
+				Score score = new Score(scoreID, rsScore.getInt("fldScore"), rsScore.getString("fldHitScore"));
+				
+				Participant p = new Participant(rs.getInt("fldParticipentID"),
+												rs.getString("fldFName"), 
+									            rs.getString("fldLName"),
+									            rs.getString("fldAgeRange"),
+									            rs.getString("fldEmail"), 
+									            score,
+									            getColor(rs.getString("fldShirtColour")),
+									            rs.getInt("fldShirtNumber"));	
+				
+			}
+			
+			return null;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+	}
+	
+	
+	private Color getColor(String colorRepresentation) {
+		String[] sColor = colorRepresentation.split(",");
+		return new Color(Integer.parseInt(sColor[0])/255, Integer.parseInt(sColor[1])/255, Integer.parseInt(sColor[2])/255);
+	}
+	
+	private String getColorString(Color c) {
+		 int r = (int) (c.getRed()*255);
+		 int g = (int) (c.getGreen()*255);
+		 int b = (int) (c.getBlue()*255);
+		 return r+","+g+","+b;
+	}
+	
+	
+	
+	
+	public List<Lane> getAllLanes() {
+
+		List<Lane> lanes = new ArrayList<>();
+
+		try {
+
+			CallableStatement csLane = connection.prepareCall("{call getLane}");
+			
+			ResultSet rs = csLane.executeQuery();
+
+			while(rs.next()){
+
+				Lane l = new Lane( rs.getInt("fldLaneNr"));
+				CallableStatement csPart = connection.prepareCall("{call getParticipantsByLaneID}");
+				int laneID = rs.getInt("fldLaneID");
+				
+				ResultSet rsPart = csPart.executeQuery();
+
+				while(rsPart.next()){
+					
+					l.addParticipant(iCtr.getParticipant(rsPart.getInt("fldParticipantID")));
+					
+				}
+
+				lanes.add(l);	
+				
+			}
+
+			return lanes;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+
 	}
 
 }
