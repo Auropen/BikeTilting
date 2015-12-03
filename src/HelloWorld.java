25c4aaf86b785352a5231cf088b54d5916d2d8f6
Auropen <sonixbock@hotmail.com>
2015-12-03 12:58:37 +0100

    Cleaning up code :squirrel:

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,8 +2,6 @@ import javafx.scene.paint.Color;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println(createSQLQuery("tblUsers", "fldFName", "bo", "fldLName", "boensen", "fldAccessLevel", 10.5));
 		
 		Color c = Color.CADETBLUE;
 		int r = (int) (c.getRed()*255);
@@ -16,21 +14,5 @@ public class HelloWorld {
 		Color c2 = new Color(r/255d, g/255d, b/255d, 1);
 		
 		System.out.println((int) (c2.getRed()*255)+","+(int) (c2.getGreen()*255)+","+(int) (c2.getBlue()*255));
-	}
-	
-	public static String createSQLQuery(String table, Object... data) {
-		String sql = "INSERT INTO " + table + "(";
-		for (int i = 0; i < data.length; i += 2) sql += data[i] + ",";
-		sql = sql.substring(0, sql.length()-1) + ") VALUES (";
-		for (int i = 1; i < data.length; i += 2) sql += ((isNumber(data[i])) ? data[i] : "'"+data[i]+"'") + ",";
-		return sql.substring(0, sql.length()-1) + ")";
-	}
-	
-	public static String selectAllSQLQuery(String table) {
-		return "SELECT * FROM " + table + ";";
-	}
-	
-	private static boolean isNumber(Object o) {
-		return o instanceof Byte || o instanceof Short || o instanceof Integer || o instanceof Long || o instanceof Double || o instanceof Float;
 	}
 }
--- src/servlet/ServletRegister.java
+++ src/servlet/ServletRegister.java
@@ -27,9 +27,7 @@ public class ServletRegister extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		//Participant p = new Participant(request.getParameter("FirstName"), request.getParameter("LastName"), request.getParameter("AgeGroup"), request.getParameter("Email"), null, null, null);
-		
-		//iCtr.addParticipantToDB(p);
+		Participant p = iCtr.addParticipantToDB(request.getParameter("FirstName"), request.getParameter("LastName"), request.getParameter("AgeGroup"), request.getParameter("Email"), null, null, null);
 		
 		response.getWriter().append("Servlet was loadet, with " + request.getParameterMap().size() + " parameters.\n "+ request.getParameter("AgeGroup"));
 	}
