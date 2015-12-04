a338c8d2b43f194f625c86278d6836f88028904c
Donpeal666 <Donpeal666@gmail.com>
2015-12-04 10:30:20 +0100

    AddParticipants

---
+++ Database/SQLCreateFourTiltingGuys.sql
@@ -0,0 +1,17 @@
+Use BikeTiltingDB
+
+
+insert into TblScore (fldHitScore,fldScore) values('0',0)
+insert into TblScore (fldHitScore,fldScore) values('1',1)
+insert into TblScore (fldHitScore,fldScore) values('2',2)
+insert into TblScore (fldHitScore,fldScore) values('3',3)
+
+insert into TblLane (fldLaneNr) values (1)
+insert into TblLane (fldLaneNr) values (2)
+insert into TblLane (fldLaneNr) values (3)
+insert into TblLane (fldLaneNr) values (4)
+
+insert into TblParticipants values('Asger', 'Jessen', '0-6', 'asger@asger.dk', 1, '255,66,88', 4, 1)
+insert into TblParticipants values('Paul', 'Jensen', '7-9', 'Paul@Yes.dk', 2, '66,44,22', 1, 2)
+insert into TblParticipants values('Kristian', 'Butch?', '10-14', 'Buch@Butch.dk', 3, '66,255,92', 3, 3)
+insert into TblParticipants values('Cornel', 'dafuq', '0-6', 'asger@asger.dk', 4, '22,22,22', 2, 4)
