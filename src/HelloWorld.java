84e2782a7561490037780d893ee1fc98a82a7e51
kornel1984 <kornelus84@gmail.com>
2015-12-04 11:01:14 +0100

    color change :+1:

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,11 @@
-import javafx.scene.paint.Color;
+
+import java.awt.Color;
+
 
 public class HelloWorld {
 	public static void main(String[] args) {
 		
-		Color c = Color.CADETBLUE;
+		Color c = Color.BLUE;
 		int r = (int) (c.getRed()*255);
 		int g = (int) (c.getGreen()*255);
 		int b = (int) (c.getBlue()*255);
@@ -11,7 +13,7 @@ public class HelloWorld {
 		
 		System.out.println(r+","+g+","+b);
 		
-		Color c2 = new Color(r/255d, g/255d, b/255d, 1);
+		Color c2 = new Color(r/255f, g/255f, b/255f, 1);
 		
 		System.out.println((int) (c2.getRed()*255)+","+(int) (c2.getGreen()*255)+","+(int) (c2.getBlue()*255));
 	}
