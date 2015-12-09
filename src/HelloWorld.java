0c57225f341450dfbd11179eb564009ca0194db0
Donpeal666 <Donpeal666@gmail.com>
2015-12-09 15:14:31 +0100

    helloWord update :sake:

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,33 @@
 
+import java.io.File;
+
+import application.Controller;
+import application.IController;
+import domain.BikeTilting;
+import domain.Participant;
 import technical.DBHandler;
 
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		DBHandler db = DBHandler.getInstance();
+		
+		IController ictr = Controller.getInstance();
+		
+		DBHandler.getProperties(new File("technicalProperties.properties"));
+		
+		BikeTilting bike = BikeTilting.getInstance();
+		
+		bike.storeDBToMemory();
+		
+		System.out.println(bike.getParticipants().size());
+		
+		System.out.println(bike.getLanes().size());
+		
+		for (Participant participant : ictr.searchParticipant("Asger", "Jessen", "", null, null)) {
+			
+			System.out.println(participant.getFName() + " > " + participant.getLName());
+			
+		}
+		
 	}
 }
