4d961f625f673ca9f8854961612b3854285cb02c
Auropen <sonixbock@hotmail.com>
2016-05-26 14:24:04 +0200

    Server Socket + Send file :dragon_face:

--- Database/SQLCreateFourTiltingGuys.sql
+++ Database/SQLCreateFourTiltingGuys.sql
@@ -20,12 +20,12 @@ insert into TblScore (fldHitScore,fldScore) values
 
 insert into TblParticipants values
 ('Asger', 'Jessen', '0-6', 'asger@asger.dk', 1, null, null, null),
-('Paul', 'Jensen', '7-9', 'Paul@Yes.dk', 2, null, null, null),
-('Kristian', 'Bock', '10-14', 'Buch@Butch.dk', 3, null, null, null),
-('Cornel', 'dafuq', '0-6', 'asger@asger.dk', 4, null, null, null),
+('Paul', 'Jensen', '7-9', 'Paul@jensen.dk', 2, null, null, null),
+('Kristian', 'Bock', '10-14', 'kristian@bock.dk', 3, null, null, null),
+('Cornel', 'Lungeanu', '0-6', 'cornel@lungeanu.com', 4, null, null, null),
 ('Simon', 'Hein', '7-9', 'hein@hein.dk', 5, null, null, null),
 ('Bjarke', 'Boisen', '0-6', 'boisen@boisen.dk', 6, null, null, null),
-('Børge', 'Børgensen', '7-9', 'luuul@email.dk', 7, null, null, null),
+('Børge', 'Børgensen', '7-9', 'boerge@email.dk', 7, null, null, null),
 ('Daniel', 'Christensen', '0-6', 'daniel@email.dk', 8, null, null, null),
 ('Markus', 'Franksen', '10-14', 'marku@franksen.com', 9, null, null, null),
 ('Simon', 'Ludvigsen', '7-9', 'simon@simon.dk', 10, null, null, null),
@@ -33,5 +33,5 @@ insert into TblParticipants values
 ('Morten', 'Skak', '0-6', 'mooo@someemail.dk', 12, null, null, null),
 ('Thomas', 'Franksen', '7-9', 'marku@franksen.com', 13, null, null, null),
 ('Sune', 'Sunesen', '7-9', 'sunekuh@sune.dk', 14, null, null, null),
-('Henrik', 'Thomsen', '0-6', 'henrikkkuh@datemail.dk', 15, null, null, null),
+('Henrik', 'Thomsen', '0-6', 'henrik@email.dk', 15, null, null, null),
 ('Line', 'Linesen', '0-6', 'line@line.dk', 16, null, null, null);
